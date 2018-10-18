b4ba9c50cac68ce4e53a7f2c00dc5563adf17bf9
brittosandro <brittosandro@gmail.com>
2018-10-18 17:43:43 -0300

    modificando script para ler e somar os coeficientes de Einstein

--- ler_fort8_n.py
+++ ler_fort8_n.py
@@ -3,6 +3,7 @@ lista_de_strings = [linha for linha in arq.readlines()]
 arq.close()
 
 n = int(input('Inf. Número de Níveis Vibracionais: '))  # n denota a quantidade de níveis vibracionais do Sistema
+nome = input('Inf. Nome do Arquivo de Output: ')        # nome arquivo de output
 
 somas_niveis_vib = []               # Lista para armazenar os valores dos níveis vibracionais
 somas_coef_einstein = []            # Lista para armazenar os valores dos coeficientes de Einstein
@@ -24,19 +25,36 @@ for j in range(0, n+1):
     somas_niveis_vib.append(total_vib)
     somas_coef_einstein.append(soma_coef_einstein)
 
-print('##############################################################################')
-print('#                                                                            #')
-print('#                        Tabela de Resultados                                #')
-print('#                        ____________________                                #')
-print('#                                                                            #')
-print('##############################################################################')
-print('')
-print(' Níveis Vibracionais |  Quantidade de cada Nível | Somas coeficientes Einstein')
-print('------------------------------------------------------------------------------')
+print(somas_niveis_vib)
+print(somas_coef_einstein)
+
+
+arq1 = open(nome, 'w')
+
+arq1.write('###################################################################################\n')
+arq1.write('#                                                                                 #\n')
+arq1.write('#                           Tabela de Resultados                                  #\n')
+arq1.write('#                           ____________________                                  #\n')
+arq1.write('#                                                                                 #\n')
+arq1.write('###################################################################################\n')
+arq1.write('\n \n ')
+
+arq1.write(' Níveis Vibracionais |  Quantidade de cada Nível | Somas coeficientes Einstein (nm)\n')
+arq1.write('-----------------------------------------------------------------------------------\n')
 
 for niveis, q_nivel in zip(range(n), somas_niveis_vib):                         # A variável q_nivel denota a soma dos níveis
-    print('{:10} {:25d} {:33.15f}'.format(niveis, q_nivel, somas_coef_einstein[niveis]))
-    print('------------------------------------------------------------------------------')
+    arq1.write('{:10} {:25d} {:33.10f}\n'.format(niveis, q_nivel, (somas_coef_einstein[niveis]*(10**9))))
+    arq1.write('-----------------------------------------------------------------------------------\n')
+
+arq1.close()
+
+print('\n************************************************************************')
+print('*                                                                      *')
+print('*   Já calculamos -> Resultados em  | {} |        *'.format(nome))
+print('*   -------------    ------------------------------------------------- *')
+print('*                                                                      *')
+print('************************************************************************\n\n')
 
 #print('Lista de Somas Niv. Vibracionais = {}'.format(somas_niveis_vib))
 #print('Lista de Soma Coef. Einstein = {}'.format(somas_coef_einstein))
+
--- ler_fort8_n1.py
+++ ler_fort8_n1.py
@@ -1,15 +1,52 @@
 arq = open('fort.8', 'r')
-lista_de_strings = [linha for linha in arq.readlines()]
+lista_de_strings = [linha.split() for linha in arq.readlines()]
 arq.close()
 
-n = int(input('Inf v.: '))
-s = []
-a0 = 0
-for i in range(len(lista_de_strings)):
-    for j in range(n):
-        niveis_vibracionais = lista_de_strings[i][11: 12]
-        if niveis_vibracionais == str(j):
-            a0 += 1
-    s.append(a0)
+const_fatiamento = 11          # Essa constante expressa o ponto do arquivo que temos interesse
+const_linha_vib1 = 2           # Essa constante expressa a posição do nível de interesse
+const_linha_coef_eins = 7      # Essa constante expressa a posição do coeficiente de einstein
 
-print('Lista de somas = {}'.format(s))
+
+n = int(input('Inf. Número de Níveis Vibracionais: '))  # n denota a quantidade de níveis vibracionais do Sistema
+nome = input('Inf. Nome do Arquivo de Output: ')        # nome arquivo de output
+
+lista_niveis = []              # Lista para armazenar as somas dos níveis vibracionais
+lista_coef_einstein = []       # Lista para armazenar as somas dos coeficientes de Einstein
+
+for i in range(n):
+    numero_niveis = 0
+    coef_eins = 0              # Contador para coeficiente de einstein
+
+    for j in range(11, len(lista_de_strings)):
+        if lista_de_strings[j][const_linha_vib1] == str(i):
+            numero_niveis += 1
+            coef_eins += float(lista_de_strings[j][const_linha_coef_eins].replace('D', 'e'))
+
+    lista_niveis.append(numero_niveis)
+    lista_coef_einstein.append(coef_eins)
+
+arq1 = open(nome, 'w')
+
+arq1.write('###################################################################################\n')
+arq1.write('#                                                                                 #\n')
+arq1.write('#                           Tabela de Resultados                                  #\n')
+arq1.write('#                           ____________________                                  #\n')
+arq1.write('#                                                                                 #\n')
+arq1.write('###################################################################################\n')
+arq1.write('\n \n ')
+
+arq1.write(' Níveis Vibracionais |  Quantidade de cada Nível | Somas coeficientes Einstein (nm)\n')
+arq1.write('-----------------------------------------------------------------------------------\n')
+
+for niveis, q_nivel in zip(range(n), lista_niveis):
+    arq1.write('{:10} {:25d} {:33.10f}\n'.format(niveis, q_nivel, lista_coef_einstein[niveis]*10**9))
+    arq1.write('-----------------------------------------------------------------------------------\n')
+
+arq1.close()
+
+print('\n************************************************************************')
+print('*                                                                      *')
+print('*   Já calculamos -> Resultados ;)                                     *'.format(nome))
+print('*   -------------    ------------------------------------------------- *')
+print('*                                                                      *')
+print('************************************************************************\n\n')
